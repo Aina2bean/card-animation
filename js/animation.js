945d873de3c80413f8b9364bb70e94ed2c94f678
Aina2bean <77223940+Aina2bean@users.noreply.github.com>
2025-10-21 23:43:00 +0900

    Add files via upload

--- js/animation.js
+++ js/animation.js
@@ -1,33 +1,45 @@
 
-const card = document.querySelector('.card');
-let hover_animation;
+const cards = document.querySelectorAll('.card');
+//let hover_animation;
 let flipped_switch = false;
 
-card.addEventListener('mouseover', () => {
-	hover_animation = gsap.to(card, {
-		rotationZ: 3,
-		x: 5,
-		duration: 0.5,
-		repeat: -1,
-		yoyo: true,
-		ease: 'sine.inOut'
-	});
-});
+console.log('cards:', cards.length);
 
-card.addEventListener('mouseout', () => {
-  hover_animation?.kill();
-  gsap.to(card, {
-		rotationZ: 0,
-		duration: 0.4,
-		ease: 'power2.out'
-	});
-});
+cards.forEach(card => {
 
-card.addEventListener('click', () => {
-  flipped_switch = !flipped_switch;
-  gsap.to(card, {
-    rotationY: flipped_switch ? 180 : 0,
-    duration: 0.8,
-    ease: 'power2.inOut',
-  });
+	card.addEventListener('mousemove', (e) => {
+
+		const rect = card.getBoundingClientRect();
+		const x = e.clientX - rect.left;
+		const y = e.clientY - rect.top;
+
+		const rotateY = ((x / rect.width) - 0.5) * 20;
+		const rotateX = ((y / rect.height) - 0.5) * -20;
+
+		gsap.to(card, {
+			rotationX: rotateX,
+			rotationY: rotateY,
+			duration: 0.3,
+			ease: 'power2.out'
+		});
+	});
+
+	card.addEventListener('mouseleave', () => {
+		gsap.to(card, {
+			rotationX: 0,
+			rotationY: 0,
+			duration: 0.5,
+			ease: 'elastic.out(1, 0.5)'
+		});
+	});
+
+	card.addEventListener('click', () => {
+		flipped_switch = !flipped_switch;
+		gsap.to(card, {
+			rotationY: flipped_switch ? 180 : 0,
+			duration: 0.8,
+			ease: 'power2.inOut',
+		});
+	});
+
 });
